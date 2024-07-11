21f9b5c39c678c57560381f68b8edfbd294bc382
huy <danghuy9229@gmail.com>
2024-07-11 08:46:16 +0700

    init project

---
+++ sparkminds-community/src/main/java/com/example/sparkminds_community/SparkmindsCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.sparkminds_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkmindsCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkmindsCommunityApplication.class, args);
+	}
+
+}
---
+++ sparkminds-community/src/test/java/com/example/sparkminds_community/SparkmindsCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sparkminds_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkmindsCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
